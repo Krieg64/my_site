48ad31c1a5ca8f7af3b68600be1e83d7b1cc1be6
Krieg64 <caiohornet2013@gmail.com>
2021-04-20 10:25:53 -0300

    Add buttons of contacts

---
+++ src/files/Photo_files/index.js
@@ -0,0 +1,47 @@
+const photos = [
+    {
+    name: "photo1",
+    destinationPhoto: "../../files/photohome.jpeg"
+    },
+    {
+    name: "photo2",
+    destinationPhoto: "../../files/photohome1.jpeg"
+    },
+    {
+    name: "photo3",
+    destinationPhoto: "../../files/photohome2.jpeg"
+    },
+    {
+    name: "photo4",
+    destinationPhoto: "../../files/project_clone-kabum.jpg"
+    },
+    {
+    name: "photo5",
+    destinationPhoto: "../../files/photohome.jpeg"
+    },
+    {
+    name: "photo6",
+    destinationPhoto: "../../files/photohome1.jpeg"
+    },
+]
+
+
+var photo = document.getElementById("root")
+var photoElement = document.getElementById(photoElement)
+
+
+photo.innerHTML = photos.map(photo => `
+    <div class="boxPhoto">
+        <input type="checkbox" class="checkPhoto" >
+        <i class="fas fa-times cancelPhoto"></i>
+        <div class="photosElement" id="photoElement"> 
+            <div class="descriptionPhoto">
+                <h2>${photo.name}</h2>
+                <p>description of photo</p>
+            </div>
+            <img src=${photo.destinationPhoto}>
+        </div>
+    </div>
+    
+`
+)
